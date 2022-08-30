51ff11fa3d9304dd939f4f3d969c5367ea4796ed
Olekka <noodarius0@gmail.com>
2022-08-30 19:21:12 +0300

    finish doctor page and subscription page --fix#2

--- js/subscription.js
+++ js/subscription.js
@@ -3,12 +3,15 @@ const details = document.querySelector(".details");
 const subscriptionButtonArray = document.querySelectorAll(".subscription__button");
 const subscriptionButtonDescriptionArray = document.querySelectorAll(".subscription__button-description");
 const payHeaderTitle = document.querySelector(".pay__header-title");
+const main = document.querySelector(".main");
 
 detailsBackButton.addEventListener("click", function(ent){       
-    details.classList.remove("details--current");    
+    details.classList.remove("details--current");
+    main.classList.remove("main--lock");    
 });
 
 subscriptionButtonArray.forEach((item, index)=> item.addEventListener("click", function(e){
     details.classList.add("details--current"); 
     payHeaderTitle.innerHTML = subscriptionButtonDescriptionArray[index].innerHTML;
+    main.classList.add("main--lock");
 }))
